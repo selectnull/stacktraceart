b60cfde0c2327b8bded7ef00ad30b814bdf1968a
Sasha Matijasic <sasha@selectnull.com>
2014-05-11 10:45:53 +0200

    initial port from c# version

---
+++ catsac.js
@@ -0,0 +1,110 @@
+﻿"use strict";
+
+/*
+TheCatInTheSacException
+Be careful the cat! No say the cat is in the sac when you have not the cat in the sac.
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaafaaaaaaafaaaaaaaaaㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaㅤㅤaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤaaaㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤaaaㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤaaaㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+
+*/
+
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤaaaㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤaaaㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤaaaㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤaaaㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤaaaㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤaaaㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤaaaㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaㅤㅤaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaㅤㅤaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaafaaaaaaafaaaaaaaaaㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaafaaaaaaafaaaaaaaaaㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaaaaaaaaaaaaaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤㅤaaaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+}
+
+/*
+function ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ() {
+}
+*/
+
+function TheCatInTheSacException () {
+    // throw new TheCatInTheSacException("Be careful the cat! No say the cat is in the sac when you have not the cat in the sac.");
+    ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ(
+        "Be careful the cat! No say the cat is in the sac when you have not the cat in the sac.");
+}
+
+module.exports.TheCatInTheSacException = TheCatInTheSacException;
